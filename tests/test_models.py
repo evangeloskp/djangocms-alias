c086d6cdcdae622ba802564e3e662a14d8575a64
Evan-Pi <vaggelispissinas@gmail.com>
2024-02-09 14:59:38 +0100

    Fix flake8 and isort

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,6 +1,5 @@
 from distutils.version import LooseVersion
 
-
 from django import get_version
 from django.contrib.sites.models import Site
 from django.urls import reverse
@@ -17,6 +16,7 @@ from .base import BaseAliasPluginTestCase
 
 DJANGO_VERSION = get_version()
 DJANGO_4_0 = LooseVersion(DJANGO_VERSION) < LooseVersion('4.1')
+
 
 class AliasModelsTestCase(BaseAliasPluginTestCase):
 
